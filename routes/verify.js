29499f453726c797fa734c7a37fb0b6b88f16077
Adebayo Ajayi <lakeside1996@live.com>
2022-05-02 04:59:04 +0200

    update verify.js

--- routes/verify.js
+++ routes/verify.js
@@ -1,33 +1,39 @@
-const { StatusCodes } = require("http-status-codes")
+const express = require('express')
 const router = express.Router()
+const { StatusCodes } = require('http-status-codes')
 const db = require('../db')
 router.get('/', async (req, res) => {
   const { id } = req.query
-  const verify =  await db.otp.findfirst({
+  const verify = await db.otp.findFirst({
     where: {
       id: { id }
     },
-    
+    select: { userId: true }
   })
-    if(!verify) return res.status(StatusCodes.BAD_REQUEST).json({error: 'Wrong code'})
-  
-    const user = await db.user.findFirst({
-      where:{
-        id:{ verify.userId}
-      }})
-      if(!user) return res.status(StatusCodes.BAD_REQUEST).json({error: 'Invalid user account'})
-      await db.user.update({
-        where:{
-          id:{ user.userId}
-        },
-        data: {
-          verified: true
-        }
-      })
-      await db.otp.delete({
-        where:{id:id}
-      })
-  res.status(200).json({message:'you have been verified'})
-}
-)
+  if (!verify)
+    return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Wrong code' })
+
+  const user = await db.user.findFirst({
+    where: {
+      id: { verify }
+    },
+    select: { id: true }
+  })
+  if (!user)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: 'Invalid user account' })
+  await db.user.update({
+    where: {
+      id: { user }
+    },
+    data: {
+      verified: true
+    }
+  })
+  await db.otp.delete({
+    where: { id: id }
+  })
+  res.status(200).json({ message: 'you have been verified' })
+})
 module.exports = router
